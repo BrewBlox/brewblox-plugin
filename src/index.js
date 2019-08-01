c28d2d1d9311ad3db081808afc79a953faccbb2b
Bob Steers <steers.bob@gmail.com>
2019-08-01 18:43:29 +0200

    simplify example use

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import BasicExample from './BasicExample';
 import StoreExample from './StoreExample';
 
-const examples = [
-  BasicExample,
-  StoreExample,
-];
-
 // This is the entry point for your plugin.
 // The install function will be called by the BrewBlox UI before starting the Vue app.
 // You can register as many features and components as you want
@@ -13,8 +8,7 @@ export default {
   install(Vue, { store }) {
     // Each example has its own install() function.
     // It's not required, but more flexible
-    examples.forEach(example => {
-      Vue.use(example, { store });
-    });
+    Vue.use(BasicExample, { store });
+    Vue.use(StoreExample, { store });
   },
 };
